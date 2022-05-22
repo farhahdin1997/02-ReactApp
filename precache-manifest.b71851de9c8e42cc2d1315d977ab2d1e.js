cb7fd2b9c8fc6c8f0d1ecca691095f77e3057c87
farhahdin1997 <farhahdin97@hotmail.com>
2022-05-22 08:54:18 +0100

    Updates

---
+++ precache-manifest.b71851de9c8e42cc2d1315d977ab2d1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c918d262f527652696a3",
+    "url": "/farhahdin1997/02-ReactApp/static/css/main.fa363b37.chunk.css"
+  },
+  {
+    "revision": "c918d262f527652696a3",
+    "url": "/farhahdin1997/02-ReactApp/static/js/main.c918d262.chunk.js"
+  },
+  {
+    "revision": "1604035f8b0b3c64294e",
+    "url": "/farhahdin1997/02-ReactApp/static/js/1.1604035f.chunk.js"
+  },
+  {
+    "revision": "eec1d57d812ad67c6626",
+    "url": "/farhahdin1997/02-ReactApp/static/js/runtime~main.eec1d57d.js"
+  },
+  {
+    "revision": "80ad2a572a76ecc529912e68c670c43a",
+    "url": "/farhahdin1997/02-ReactApp/index.html"
+  }
+];
